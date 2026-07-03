34d9f323a765883df3f4541e7ea5b94caf9dd865
nurdina abd manan <nurdinaabdullmanan@gmail.com>
2026-07-03 11:05:22 +0800

    Updated code

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ st.markdown(f"""
 
     /* BACKGROUND UTAMA */
     .stApp, [data-testid="stAppViewContainer"], [data-testid="stHeader"] {{
-        background: linear-gradient(135deg, #2e2640 0%, #FFB703 100%) !important; 
+        background: linear-gradient(135deg, #2e2640 0%, #2EC4B6 100%) !important; 
         color: #f8fafc !important;
     }}
     
